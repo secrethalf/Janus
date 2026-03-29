62cb610da6985d09659ff2fb158148d02ccfe3c6
secrethalf <158239348+secrethalf@users.noreply.github.com>
2026-03-30 00:42:59 +0700

    Delete start.sh

--- start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-PORT=${PORT:-8088}
-mkdir -p /usr/local/etc/janus
-printf '[general]\nip = 0.0.0.0\nport = %s\nadmin_port = 7088\nbase_path = /janus\n' "$PORT" > /usr/local/etc/janus/janus.transport.http.cfg
-exec janus
