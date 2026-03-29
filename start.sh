dc1b0c1df4176e10cf153720701dd5c7c999a005
secrethalf <158239348+secrethalf@users.noreply.github.com>
2026-03-30 00:39:52 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
 PORT=${PORT:-8088}
-cat > /usr/local/etc/janus/janus.transport.http.cfg << EOF
-[general]
-ip = 0.0.0.0
-port = $PORT
-admin_port = 7088
-base_path = /janus
-EOF
+mkdir -p /usr/local/etc/janus
+printf '[general]\nip = 0.0.0.0\nport = %s\nadmin_port = 7088\nbase_path = /janus\n' "$PORT" > /usr/local/etc/janus/janus.transport.http.cfg
 exec janus
